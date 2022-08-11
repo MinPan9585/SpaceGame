08e22c1988b67f006eded422f6bd10a1a98ec919
EnweiJin <enweijin2005@gmail.com>
2022-08-11 23:06:55 +0800

    updated random generation

--- space game thing/Assets/scripts/monospeed.cs
+++ space game thing/Assets/scripts/monospeed.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class monospeed : MonoBehaviour
 {
     public Rigidbody2D body;
-    public float ratio;
-    public Rigidbody2D playerbody;
+    //public float ratio;
+    //public Rigidbody2D playerbody;
 
     private void Awake()
     {
-        //body.velocity = new Vector2(playerbody.velocity.x - 15, 0);
+        body.velocity = new Vector2(20, 0);
     }
     private void Update()
     {
-        body.velocity = new Vector2(ratio * playerbody.velocity.x, 0);
+        //body.velocity = new Vector2(ratio * playerbody.velocity.x, 0);
     }
 }
---
+++ space game thing/Assets/scripts/spawnSwarm.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnSwarm : MonoBehaviour
+{
+
+    public GameObject gate;
+    public GameObject[] presets;
+    public Transform selftransform;
+
+    private int randomint;
+
+    private void Awake()
+    {
+        selftransform = this.transform;
+        randomint = Random.Range(1, 5);
+        //Instantiate(presets[randomint], selftransform.position, selftransform.rotation);
+        Debug.Log(randomint);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            //Debug.Log("crossed");
+            Instantiate(gate, new Vector3(selftransform.position.x + 128, 0, 0), new Quaternion (0,0,0,0));
+        }
+    }
+}
